f830035ce4707b664eedc4b3298865e22328b30f
habvi <a2231250601h@yahoo.co.jp>
2022-03-18 16:20:51 +0900

    create initial db

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+# //// : relative pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# initialize database
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    # reference each id
+    id = db.Column(db.Integer, primary_key=True)
+    # hold each tasks
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # return task id just created
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 
 @app.route('/')
